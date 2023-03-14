e188393362ad7c204e9ec7d362e7e15472dcb584
Morten <7154135+modernpretense@users.noreply.github.com>
2023-03-15 00:24:14 +0100

    lots of work since last commit, works great

--- script.js
+++ script.js
@@ -30,20 +30,25 @@ const setDisplay = (number) => {
 
 // Global variables
 let storedNumber = 0;
-let activeNumber;
+let activeNumber = 0;
 let operatorPressed = false; // Flag to see if an operator-button has been pressed. Is cleared by next nummeral input.
 let equalPressed = false;
 let activeOperator; // 'add, 'subtract', 'multiply', 'divide', 'reset'
+let storedOperator;
+let activeComma = false;
+let isCommaUsed = false;
 
 // Buttons
 
 const numBtnPress = function (btnNumber) {
     checkOperatorPressed();
+    let aNumLength = activeNumber.toString().length;
     if (activeNumber === undefined || activeNumber === 0 || equalPressed === true) {
         activeNumber = Number(`${btnNumber}`);
         equalPressed = false;
-    } else {
-    activeNumber = Number(`${activeNumber}${btnNumber}`);
+    } else if (aNumLength < 10) {
+        checkInsertComma();
+        activeNumber = Number(`${activeNumber}${btnNumber}`);
     }
     setDisplay(activeNumber);
 }
@@ -53,6 +58,9 @@ const cBtnPress = function () {
     storedNumber = 0;
     activeOperator = 'reset';
     operatorPressed = false;
+    activeComma = false;
+    isCommaUsed = false;
+    removeMarkOperator();
     setDisplay(activeNumber);
 }
 
@@ -60,14 +68,21 @@ const acBtnPress = function () {
     activeNumber = 0;
     storedNumber = 0;
     activeOperator = 'reset';
+    storedOperator = 'reset';
     operatorPressed = false;
     equalPressed = false;
+    activeComma = false;
+    isCommaUsed = false;
+    removeMarkOperator();
     setDisplay(activeNumber);
 }
 
 const setActiveOperator = function (operatorInput) {
+    storedOperator = activeOperator;
     activeOperator = operatorInput;
     operatorPressed = true;
+    isCommaUsed = false;
+    markOperator(operatorInput);
 }
 
 const equalsBtnPress = function () {
@@ -80,19 +95,67 @@ const equalsBtnPress = function () {
     }
     storedNumber = 0;
     activeOperator = 'reset';
+    removeMarkOperator();
     equalPressed = true;
-    setDisplay(activeNumber);
+    isCommaUsed = false;
+    setDisplay(activeNumberOutput(activeNumber));
 }
 
 // checkOperatorPressed is attatched to numberbuttons to make chaining opearations possible wihtout using equal-button first.
 const checkOperatorPressed = function () {
     if (operatorPressed === true) {
         if (storedNumber !== 0 && activeNumber !== 0) {
-            storedNumber = operate(`${activeOperator}`, storedNumber, activeNumber);
+            storedNumber = operate(`${storedOperator}`, storedNumber, activeNumber);
         } else {
             storedNumber = activeNumber;
         }
     activeNumber = 0;
     operatorPressed = false;
     }
+}
+
+const commaBtnPress = function () {
+    if (activeNumber !== 0) {
+    activeComma = true;
+    setDisplay(`${activeNumber}.`)
+    }
+}
+
+const checkInsertComma = function () {
+    if (activeComma === true && isCommaUsed === false) {
+        activeNumber = `${activeNumber}.`;
+        activeComma = false;
+        isCommaUsed = true;
+    }
+}
+
+// Event listener to show if operator is active
+
+
+const markOperator = function (operator) {
+    removeMarkOperator();
+    const element = document.getElementById(`${operator}`);
+    element.classList.add("markedbtn");
+}
+
+const removeMarkOperator = function () {
+    const operatorButtons = document.querySelectorAll('.operatorbtn');
+    operatorButtons.forEach((button) => {
+        button.classList.remove('markedbtn');
+    });
+}
+
+const activeNumberOutput = function (inputnumber) {
+    let number = Number(inputnumber.toFixed(2));
+    let numberLength = number.toString().length;
+    
+    if (isFinite(number) === false) {
+        return 'delt paa 0';
+    } else if (numberLength <= 10) {
+        return number;
+    } else {
+        return number.toExponential(2);
+    }
+
+
 }
